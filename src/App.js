c2816718260cec8586196e022b6e045f013718bd
Gunkar Singh <gunkar.singh@neofinancial.com>
2021-10-30 18:32:15 -0600

    Initial project setup

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react"
+import "./App.css"
+
+import HomePage from "./pages/homepage/HomepageComponent"
+
+function App() {
+    return <HomePage />
+}
+
+export default App
---
+++ src/pages/homepage/HomepageComponent.js
@@ -0,0 +1,8 @@
+import React from "react"
+import "./homepageStyles.scss"
+
+const HomePage = () => {
+    return <div>HomePage goes here test</div>
+}
+
+export default HomePage
