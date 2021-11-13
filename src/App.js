81801b041907f69958d541ffc77e9528690500f7
Gunkar Singh <gunkar.singh@neofinancial.com>
2021-11-12 23:20:39 -0700

    Added code to store user in firebase database

--- src/App.js
+++ src/App.js
@@ -8,16 +8,26 @@ import HomePage from "./pages/homepage/HomePage"
 import ShopPage from "./pages/shop/ShopPage"
 import SignInSignUpPage from "./pages/signin-signup/SignInSignUpPage"
 
-import { auth } from "./firebase/firebase.utils"
+import {
+    auth,
+    createUserProfileDocument
+} from "./firebase/firebase.utils"
 
 function App() {
     const [currentUser, setCurrentUser] = useState(null)
 
     /* for later => extract code below into custom hook */
     useEffect(() => {
-        const unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-            user ? setCurrentUser(user) : setCurrentUser(null)
-        })
+        const unsubscribeFromAuth = auth.onAuthStateChanged(
+            async user => {
+                if (user) {
+                    createUserProfileDocument(user)
+                    setCurrentUser(user)
+                } else {
+                    setCurrentUser(null)
+                }
+            }
+        )
 
         return () => {
             unsubscribeFromAuth()
